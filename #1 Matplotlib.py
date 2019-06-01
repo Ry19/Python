339e78aca4de7dd52ecf0380ca150cc477725be0
Yash Raghuwansi <yashraghuwansi@Yashs-MacBook-Air.local>
2019-05-31 23:35:49 -0500

    added new files

--- #1 Matplotlib.py
+++ #1 Matplotlib.py
@@ -3,3 +3,23 @@ days=(1,2,3,4,5,6,7)
 prices=(10,20,30,40,50,60,70)
 plt.plot(days,prices, color="red", linestyle="--")
 plt.show()
+
+
+plt.plot(year, computer_science, color='red', label='Computer Science') 
+plt.plot(year, physical_sciences, color='blue', label='Physical Sciences')
+plt.legend(loc='lower right')
+
+# Compute the maximum enrollment of women in Computer Science: cs_max
+cs_max = computer_science.max()
+
+# Calculate the year in which there was maximum enrollment of women in Computer Science: yr_max
+yr_max = year[computer_science.argmax()]
+
+# Add a black arrow annotation
+plt.annotate('Maximum', xy=(yr_max, cs_max), xytext=(yr_max+5, cs_max+5), arrowprops=dict(facecolor='black'))
+
+# Add axis labels and title
+plt.xlabel('Year')
+plt.ylabel('Enrollment (%)')
+plt.title('Undergraduate enrollment of women')
+plt.show()
